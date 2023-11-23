eaa7d8fa311e5bf53489cd94fec2123e285010f8
Masonville17 <masonville17@gmail.com>
2023-11-23 00:19:14 -0700

    organizing code, iptables first, then establish route, added a few comments for readability

--- start.sh
+++ start.sh
@@ -1,22 +1,7 @@
 #!/bin/bash
 VPN_INTERFACE="tun0"
 
-wget https://configs.ipvanish.com/configs/configs.zip
-unzip configs.zip
-rm configs.zip
-
-readarray -t countries < exclude_countries
-
-for country in "${countries[@]}"
-do
-    echo "Removing files with pattern *-$country-*"
-    rm -rf *-"$country"-*
-done
-
-OVPN_FILE=$(find . -name "*.ovpn" | shuf -n 1)
-echo "Today, we'll connect to $OVPN_FILE"
-
-sed -i '/keysize/d' "${OVPN_FILE}"
+echo "Adding iptable rules"
 
 # Local ports to exclude from VPN
 iptables -A OUTPUT -o tun0 -p tcp --dport 7657 -j DROP
@@ -24,14 +9,33 @@ iptables -A OUTPUT -o tun0 -p tcp --dport 4445 -j DROP
 iptables -A OUTPUT -o tun0 -p tcp --dport 4444 -j DROP
 iptables -t nat -A POSTROUTING -o tun0 -j MASQUERADE
 
+# downloading and configuring openvpn files / connection
+
+# download and inflate, remove zipfile and exclude countries
+wget https://configs.ipvanish.com/configs/configs.zip
+unzip configs.zip
+rm configs.zip
+readarray -t countries < exclude_countries
+for country in "${countries[@]}"
+do
+    echo "Removing files with pattern *-$country-*"
+    rm -rf *-"$country"-*
+done
+
+# select random ovpn file and establish connection
+OVPN_FILE=$(find . -name "*.ovpn" | shuf -n 1)
+echo "Establishing Connection with $OVPN_FILE"
+sed -i '/keysize/d' "${OVPN_FILE}"
 openvpn --config "${OVPN_FILE}" --auth-user-pass pass & 
 vpn_pid=$!
 
-sleep 45  # Wait for VPN to establish connection
+# Wait for VPN to establish connection and check if connection is established
+sleep 45
 if ip a show tun0 up > /dev/null 2>&1; then
     echo "VPN is connected."
 else
     echo "VPN connection failed."
+    exit 1
 fi
 
 echo "installing and configuring i2p"
